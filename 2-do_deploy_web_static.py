2d27bc0e37ee0cb72c6b60b75cdf669f5d103b7a
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-25 23:40:22 -0300

    Add task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack.
+"""
+from fabric.api import local, put, run, env
+from datetime import datetime
+from os.path import getsize, exists
+from os import mkdir
+
+env.hosts = ['3.91.220.140', '54.221.177.99']
+
+
+def do_deploy(archive_path):
+    """
+    Deploy the package tgz file to both servers
+    """
+    try:
+        archive = archive_path.split('/')[-1]
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        current = '/data/web_static/current'
+        put(archive_path, '/tmp')
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(path, current))
+        print('New version deployed!')
+        return True
+    except Exception:
+        return False
