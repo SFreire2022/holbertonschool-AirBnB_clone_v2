6b9404a30b4e1e80de51397e4c71eaefe2026c90
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-25 21:04:16 -0300

    Return 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,7 @@ apt-get -y update
 apt-get -y install nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "
-<html>
+echo "<html>
  <head>
    <title>
    Holberton School (title)
@@ -18,5 +17,6 @@ echo "
  </html>" > /data/web_static/releases/test/index.html
 ln -sfn /data/web_static/current /data/web_static/releases/test/
 chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a \tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\tautoindex off;\n}' /etc/nginx/sites-available/default
 service nginx restart
+exit 0
