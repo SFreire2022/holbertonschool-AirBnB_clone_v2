9442f6effc4735265721cfc75109fcce43fb5c93
SFreire2022 <sebastiancazenave@gmail.com>
2022-09-02 23:53:58 -0300

    Add task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -71,7 +71,7 @@ def odd_or_even(n):
     display a HTML page only if n is an integer:
     H1 tag: “Number: n is even|odd” inside the tag BODY
     """
-    return render_template('6-number_odd_or_even.html', n=escape(n))
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == "__main__":
